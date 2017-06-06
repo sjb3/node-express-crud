135f89a628e2698b5526171692029941125a631c
Sung J Byun <sungjbyun@D-108-179-146-213.dhcp4.washington.edu>
2017-06-05 21:41:06 -0700

    initial, basic server file set

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get(('/'), (req, res) => {
+    res.send('hello!');
+    });
+
+app.listen(3000, () => {
+    console.log('Server running <@))>< at 3000');
+   });
