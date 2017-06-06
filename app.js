f0a45bd8b018f6503800d5babce9410db556cd37
Sung J Byun <sungjbyun@D-173-250-248-205.dhcp4.washington.edu>
2017-06-06 15:11:48 -0700

    OMG PUG is crazy

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+//init App
 const app = express();
 
+//load View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//home routes
 app.get('/', (req, res) => {
-    res.send('hello!');
+//make a static instead mongoDB for now
+    var articles = [
+    {
+        id: 1,
+        title: 'Article One',
+        author: 'VBrad Pitt',
+        body: 'This is BP'
+    },
+    {
+        id: 2,
+        title: 'Article Two',
+        author: 'Angie Jolly',
+        body: 'This is AJ'
+    },
+    {
+        id: 3,
+        title: 'Article Three',
+        author: 'Lucien Poletta',
+        body: 'This is LP'
+    }
+];
+    res.render('index', {
+        title: 'HOME',
+        articles: articles
     });
+});
+
+//add route
+app.get('/articles/add', (req, res) => {
+    res.render('add_article', {
+        title: 'Add Article'
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running <@))>< at 3000');
